8a1945f845b8ca196d7c8fb6d65aec63f2d19960
Gregory Weiner <gregory.weiner@aya.yale.edu>
2016-07-12 23:02:01 +0700

    Created camera class.

--- DX11_Framework_custom/Camera.h
+++ DX11_Framework_custom/Camera.h
@@ -1,1 +1,46 @@
+/*****************************************************************************************
+ *This camera class will to let DirectX 11 know from where and also how we are viewing   *
+ *the scene. The camera class will keep track of where the camera is and its current     *
+ *rotation. It will use the position and rotation information to generate a view matrix  *
+ *which will be passed into the HLSL shader for rendering.                               *
+ *                                                                                       *
+ *****************************************************************************************/
 #pragma once
+
+////////////////////////////////////////////////////////////////////////////////
+// Filename: camera.h
+////////////////////////////////////////////////////////////////////////////////
+
+//////////////
+// INCLUDES //
+//////////////
+#include <directxmath.h>
+using namespace DirectX;
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: Camera
+////////////////////////////////////////////////////////////////////////////////
+class Camera
+{
+public:
+	Camera();
+	Camera(const Camera&);
+	~Camera();
+
+    //  set the position and rotation of the camera object. 
+	void SetPosition(float, float, float);
+	void SetRotation(float, float, float);
+
+	XMFLOAT3 GetPosition();
+	XMFLOAT3 GetRotation();
+
+    // create the view matrix based on the position and rotation of the camera. 
+	void Render();
+    // retrieve the view matrix from the camera object so that the shaders can use it for rendering.
+	void GetViewMatrix(XMMATRIX&);
+
+private:
+	float m_positionX, m_positionY, m_positionZ;
+	float m_rotationX, m_rotationY, m_rotationZ;
+	XMMATRIX m_viewMatrix;
+};
