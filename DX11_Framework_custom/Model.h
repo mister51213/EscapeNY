3b82eb70998eef976383017825c3f9da393d381d
Gregory Weiner <gregory.weiner@aya.yale.edu>
2016-07-22 21:07:48 +0700

    Updated Model.h to be parent.

--- DX11_Framework_custom/Model.h
+++ DX11_Framework_custom/Model.h
@@ -11,41 +11,83 @@
 // INCLUDES //
 //////////////
 #include "Graphics.h"
+#include "PrimitiveMaker.h"
 using namespace DirectX;
 
-////////////////////////////////////////////////////////////////////////////////
+////////////////////////
 // Class name: Model
-////////////////////////////////////////////////////////////////////////////////
+////////////////////////
 class Model
 {
-private:
-    // Definition of the vertex type that will be used with the vertex buffer
-	// in this class.  
-    // *This typedef must match the layout in the ColorShader class*
     
-	struct VertexType
-	{
-		XMFLOAT3 position;
-		XMFLOAT4 color;
-	};
-
 public:
 	Model();
 	Model(const Model&);
 	~Model();
-    
-	bool Initialize(ID3D11Device*);
-	void Render(ID3D11DeviceContext*);
-	void GetWorldMatrix( XMMATRIX &WorldMatrix );
-	int GetIndexCount();
 
-private:
-	bool InitializeBuffers(ID3D11Device*);
-    // The Render function puts the model geometry on the video card to prepare it for drawing by the color shader.
-	void RenderBuffers(ID3D11DeviceContext*);
-  
-private:
+    // This pure virtual initializer takes into account different types of models
+    // (textured and primitive, for example)
+	virtual bool Initialize( const PrimitiveMaker &PrimMaker, const Graphics &Gfx ) = 0;
+	virtual std::vector<D3D11_INPUT_ELEMENT_DESC> GetInputElementDescriptions() const = 0;
+
+	XMMATRIX GetWorldMatrix()const;
+	DWORD GetVertexCount()const;
+	DWORD GetIndexCount()const;
+	ID3D11Buffer *GetVertexBuffer()const;
+	ID3D11Buffer *GetIndexBuffer()const;
+	UINT GetStride()const;
+
+protected:
+	template<class VertexType>
+    bool initializeBuffers(VertexType *pVertexData, 
+        DWORD *pIndexData, 
+        const Graphics &Gfx)
+    {
+        // Set up the description of the static vertex buffer.
+        D3D11_BUFFER_DESC vertexBufferDesc{};
+        vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+        vertexBufferDesc.ByteWidth = sizeof(VertexType) * m_vertexCount;
+        vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+        vertexBufferDesc.CPUAccessFlags = 0;
+        vertexBufferDesc.MiscFlags = 0;
+        vertexBufferDesc.StructureByteStride = 0;
+
+        // Give the subresource structure a pointer to the vertex data.
+        D3D11_SUBRESOURCE_DATA vertexData{};
+        vertexData.pSysMem = pVertexData;
+        vertexData.SysMemPitch = 0;
+        vertexData.SysMemSlicePitch = 0;
+
+        // Now create the vertex buffer.
+        auto pDevice = Gfx.GetDirect3D()->GetDevice();
+        HRESULT result = pDevice->CreateBuffer(&vertexBufferDesc, &vertexData, m_vertexBuffer.GetAddressOf());
+        RETURN_IF_FAILED(result);
+
+        // Set up the description of the static index buffer.
+        D3D11_BUFFER_DESC indexBufferDesc{};
+        indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+        indexBufferDesc.ByteWidth = sizeof(DWORD) * m_indexCount;
+        indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+        indexBufferDesc.CPUAccessFlags = 0;
+        indexBufferDesc.MiscFlags = 0;
+        indexBufferDesc.StructureByteStride = 0;
+
+        // Give the subresource structure a pointer to the index data.
+        D3D11_SUBRESOURCE_DATA indexData{};
+        indexData.pSysMem = pIndexData;
+        indexData.SysMemPitch = 0;
+        indexData.SysMemSlicePitch = 0;
+
+        // Create the index buffer.
+        result = pDevice->CreateBuffer(&indexBufferDesc, &indexData, m_indexBuffer.GetAddressOf());
+        RETURN_IF_FAILED(result);
+
+        return true;
+    }
+    
+protected:
+	XMMATRIX m_worldMatrix;
 	comptr<ID3D11Buffer> m_vertexBuffer, m_indexBuffer;
 	int m_vertexCount, m_indexCount; // keep track of the size of each buffer
-	XMMATRIX m_worldMatrix;
+	UINT m_Stride;
 };
