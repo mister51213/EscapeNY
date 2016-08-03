512265cb05e127d867a44d85a4a2e61169fbe3de
albinopapa <rvpjosh@yahoo.com>
2016-08-03 00:29:52 -0500

    Added more overloaded operator functions for XMFLOAT3 data type and a few vector math operations and some constants all in Utilities.h

--- DX11_Framework_custom/Utilities.h
+++ DX11_Framework_custom/Utilities.h
@@ -31,22 +31,87 @@ else int a = 0
 // Vector Math Operator Overloads
 ///////////////////////////////////////////////////
 
-//XMFLOAT3 operator += (XMFLOAT3& v1, XMFLOAT3& v2)
-//{
-//    v1.x += v2.x;
-//    v1.y += v2.y;
-//    v1.z += v2.z;
-//    return v1;
-//}
+// Keep operator overloads out of namespace so we can access them
+// without having to declare: using namespace
+inline XMFLOAT3 operator+(const XMFLOAT3 &V1, const XMFLOAT3& V2)
+{
+	return XMFLOAT3(V1.x + V2.x, V1.y + V2.y, V1.z + V2.z);
+}
+inline XMFLOAT3 operator-(const XMFLOAT3 &V1, const XMFLOAT3& V2)
+{
+	return XMFLOAT3(V1.x - V2.x, V1.y - V2.y, V1.z - V2.z);
+}
+inline XMFLOAT3 operator*(const XMFLOAT3 &V, const float S)
+{
+	return XMFLOAT3(V.x * S, V.y * S, V.z * S);
+}
+inline XMFLOAT3 operator/(const XMFLOAT3 &V, const float S)
+{
+	const float recipScalar = 1.f / S;
+	return XMFLOAT3(V.x * recipScalar, V.y * recipScalar, V.z * recipScalar);
+}
 
-// redundant?
-//XMFLOAT3 operator + (XMFLOAT3& v1, float cubeMod)
-//{
-//    v1.x += cubeMod;
-//    v1.y += cubeMod;
-//    v1.z += cubeMod;
-//    return v1;
-//}
+inline XMFLOAT3 &operator+= (XMFLOAT3& V1, const XMFLOAT3& V2)
+{
+	V1 = V2 + V1;
+	return V1;
+}
+inline XMFLOAT3 &operator-= (XMFLOAT3& V1, const XMFLOAT3& V2)
+{
+	V1 = V2 - V1;
+	return V1;
+}
+inline XMFLOAT3 &operator*= (XMFLOAT3 &V, const float S)
+{
+	V = V * S;
+	return V;
+}
+inline XMFLOAT3 &operator/= (XMFLOAT3 &V, const float S)
+{
+	V = V / S;
+	return V;
+}
+
+// PI is equal to 180 degrees
+constexpr float PI = 3.141592654f;
+// TwoPI is equal to 360 degrees
+constexpr float TwoPI = 2.f * PI;
+// PIDivFour is equal to 45 degrees
+constexpr float PIDivFour = PI / 4.f;
+// PIDivTwo is equal to 90 degrees
+constexpr float PIDivTwo = PI / 2.f;
+// Radian is 1 radian, multiply degrees by radian to convert 
+// degrees to radians for rotation functions
+constexpr float radian = PI / 180.f;
+
+inline XMFLOAT3 CrossProduct(const XMFLOAT3 &V1, const XMFLOAT3 &V2)
+{
+	return
+	{
+		(V1.y * V2.z) - (V1.z * V2.y),
+		(V1.z * V2.x) - (V1.x * V2.z),
+		(V1.x * V2.y) - (V1.y * V2.x)
+	};
+}
+inline float DotProduct(const XMFLOAT3 &V1, const XMFLOAT3 &V2)
+{
+	return (V1.x * V2.x) + (V1.y * V2.y) + (V1.z * V2.z);
+}
+inline float Magnitude(const XMFLOAT3 &V)
+{
+	return sqrtf(DotProduct(V, V));
+}
+inline float Length(const XMFLOAT3 &V1, const XMFLOAT3 &V2)
+{
+	return Magnitude(V2 - V1);
+}
+inline XMFLOAT3 Normalize(const XMFLOAT3 &V)
+{
+	// Optimization
+	float recipricalLength = 1.0f / Magnitude(V);
+	return V * recipricalLength;
+}
+
 
 ////////////////////////////////////////////////////////////////
 // KEY FUNCTION FOR GETTING WORLD MATRIX
