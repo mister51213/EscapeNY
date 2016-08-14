6db5542b3c2e4df177f8acd69fcd92ed9d6972da
albinopapa <rvpjosh@yahoo.com>
2016-08-13 23:34:58 -0500

    Moved the Grid3D algorithm to cpp file, commented out the old inherited MakePattern function

--- DX11_Framework_custom/Algorithm_Grid3D.cpp
+++ DX11_Framework_custom/Algorithm_Grid3D.cpp
@@ -1,31 +1,87 @@
 #include "Algorithm_Grid3D.h"
 
-vector<Actor> Algorithm_Grid3D::MakePattern( int numActors )
+///////////////////////////////////////
+// Old inherited function
+
+//vector<Actor> Algorithm_Grid3D::MakePattern( int numActors )
+//{
+//	numActors -= ( numActors % 25 );
+//
+//	const int m_numColumns = numActors / 25;
+//	const int m_numRows = numActors / 25;
+//	const int m_numZ = numActors / 25;
+//
+//	vector<Actor> actorsSUB;
+//	actorsSUB.reserve( numActors );
+//
+//	ModelSpecs_W specs = {};
+//	specs.scale = { .01f, .01f, .01f };
+//	const float spacing = 15.f;
+//	for( float i = 0; i < m_numColumns; i++ )
+//	{
+//		specs.position.x = i * spacing;
+//		for( float j = 0; j < m_numRows; j++ )
+//		{
+//			specs.position.y = j * spacing;
+//			for( float k = 0; k < m_numZ; k++ )
+//			{
+//				specs.position.z = k * spacing;			
+//				actorsSUB.push_back( Actor( specs, AsphaltFresh, ModelSpecs_L() ) );
+//			}
+//		}
+//	}
+//	return actorsSUB;
+//}
+
+///////////////////////////////////////
+
+Algorithm_Grid3D::Algorithm_Grid3D()
+	:
+	Algorithm( nullptr )
 {
-	numActors -= ( numActors % 25 );
+}
 
-	const int m_numColumns = numActors / 25;
-	const int m_numRows = numActors / 25;
-	const int m_numZ = numActors / 25;
+vector<Actor_NPC> Algorithm_Grid3D::MakePatternNPC( int numActors )
+{
+	int m_numColumns;
+	int m_numRows;
+	int m_numZ;
 
-	vector<Actor> actorsSUB;
+	if( numActors != 0 && numActors % 25 == 0 )
+	{
+		m_numColumns = numActors / 25;
+		m_numRows = numActors / 25;
+		m_numZ = numActors / 25;
+	}
+	else
+	{
+		numActors -= ( numActors % 25 );
+		m_numColumns = numActors / 25;
+		m_numRows = numActors / 25;
+		m_numZ = numActors / 25;
+	}
+	vector<Actor_NPC> actorsSUB;
 	actorsSUB.reserve( numActors );
 
-	ModelSpecs_W specs = {};
-	specs.scale = { .01f, .01f, .01f };
-	const float spacing = 15.f;
+	ModelSpecs_W specs = { { 0.f, 0.f, 0.f },{ 0.f,0.f,0.f },{ .3f,.4f,.5f } };
 	for( float i = 0; i < m_numColumns; i++ )
 	{
-		specs.position.x = i * spacing;
 		for( float j = 0; j < m_numRows; j++ )
 		{
-			specs.position.y = j * spacing;
 			for( float k = 0; k < m_numZ; k++ )
 			{
-				specs.position.z = k * spacing;			
-				actorsSUB.push_back( Actor( specs, AsphaltFresh, ModelSpecs_L() ) );
+				specs.position.x = i*15.f;
+				specs.position.y = j*15.f;
+				specs.position.z = k*15.f;
+				specs.scale.x += .01f;
+				specs.scale.y += .015f;
+				specs.scale.z += .02f;
+
+				int index = actorsSUB.size();
+				actorsSUB.push_back( Actor_NPC(/*m_pInput, */specs, AsphaltFresh, ModelSpecs_L() ) );
 			}
 		}
 	}
+
 	return actorsSUB;
 }
