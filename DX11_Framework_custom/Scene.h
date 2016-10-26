5e2781fffb195267a6ffd023f144eb6f9c69ccdd
Gregory Weiner <gregory.weiner@aya.yale.edu>
2016-10-26 14:07:00 +0700

    .

--- DX11_Framework_custom/Scene.h
+++ DX11_Framework_custom/Scene.h
@@ -17,8 +17,8 @@ public:
 	~Scene();
 
 public:
-	vector<Actor> m_actors_dynamic; // player, NPCs
-	vector<Actor> m_actors_static; // trees, walls, scenery, etc
+	vector<Actor> m_actors_dynamic; // player, NPCs *Use UpdateState() function
+	vector<Actor> m_actors_static; // trees, walls, scenery, etc *SAME STATE all the time
 	// TODO: look into how to animate static actors like this
 	vector<Camera> m_cameras; // dynamic / fixed, different camera angles
 	vector<Texture> m_texturePool;
